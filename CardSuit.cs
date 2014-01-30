0198e10077815896783463d489731e7488b801f7
Stoyan <stoyan_velkushanov@yahoo.com>
2014-01-30 19:37:44 +0100

    CardSuit.cs

---
+++ CardSuit.cs
@@ -0,0 +1,12 @@
+
+namespace JustBlackJack
+{
+    public enum CardSuit
+    {
+        Clubs = 1,      // ♣  kupa
+        Diamonds = 2,   // ♦  karo
+        Spades = 3,     // ♠  pika
+        Hearts = 4      // ♥  spatia
+        
+    }
+}
